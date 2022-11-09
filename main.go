cb76214579d75f52697b61cc0c388c0fa80de33b
Isma1306 <isma1306@gmail.com>
2022-11-09 21:17:25 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Article struct {
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+var Articles []Article
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+func handleRequests() {
+	r := mux.NewRouter().StrictSlash(true)
+
+	r.HandleFunc("/", homePage)
+	r.HandleFunc("/all", returnAllArticles)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":2550", nil))
+}
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllArticles")
+	json.NewEncoder(w).Encode(Articles)
+}
+
+func main() {
+	Articles = []Article{
+		{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		{Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+	}
+	handleRequests()
+}
